8b9a86f5598ca51bb0c26db2a93366a281292db1
Arthur Walsh <walsh.arthur@outlook.com>
2021-03-21 19:38:57 +0100

    Added Splash Screen

---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,97 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:tomasfamilyapp/screens/HomePage.dart';
+
+class SplashScreen extends StatefulWidget {
+  final Color backgroundColor = Color.fromRGBO(19, 60, 109, 255);
+  final TextStyle styleTextUnderTheLoader = TextStyle(
+      fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.white);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  String _versionName = 'V0.0.1';
+  final splashDelay = 5;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loadWidget();
+  }
+
+  _loadWidget() async {
+    var _duration = Duration(seconds: splashDelay);
+    return Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context) => MyHomePage(title: 'Tomas')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF133C6D),
+      body: InkWell(
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 7,
+                  child: Container(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/images/TomasLogoLight.png',
+                        height: 400,
+                        width: 400,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                      ),
+                    ],
+                  )),
+                ),
+                Expanded(
+                  child: Column(
+                    children: <Widget>[
+                      CircularProgressIndicator(
+                        valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                      Container(
+                        height: 10,
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Spacer(),
+                            Text(_versionName,
+                            style: TextStyle(color: Colors.white),),
+                            Spacer(
+                              flex: 4,
+                            ),
+                            Text('Loading...',
+                            style: TextStyle(color: Colors.white)),
+                            Spacer(),
+                          ])
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
